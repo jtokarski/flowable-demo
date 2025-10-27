28ba638eecad8ff0c19dc2d87ff8bf55913c599b
Jozef Tokarski <jozef.tokarski@gmail.com>
2025-10-27 06:26:20 +0100

    019-accounting-multipoc kickoff

--- 019-accounting-multipoc/src/test/java/org/defendev/flowable/demo/multipoc/TestMemoryUtil.java
+++ 019-accounting-multipoc/src/test/java/org/defendev/flowable/demo/multipoc/TestMemoryUtil.java
@@ -1,4 +1,17 @@
 package org.defendev.flowable.demo.multipoc;
 
+import static java.lang.String.format;
+
+
+
 public class TestMemoryUtil {
+
+    public static void printLabel(Object value, String label) {
+        System.out.println(format("--- --- %s : %s", label, value));
+    }
+
+    public static void printBar() {
+        System.out.println("--- -------------------------------------------- ---");
+    }
+
 }
