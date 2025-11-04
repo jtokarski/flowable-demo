96b36973d60890b74be33fa8be4a64e012c32d38
Jozef Tokarski <jozef.tokarski@gmail.com>
2025-11-04 20:01:33 +0100

    009-spring-gateway-reactive - In case for terminated session - XHRs to get 401s instead of 302s

--- 009-spring-gateway-reactive/src/main/java/org/defendev/spring/cloud/gateway/demo/config/SecurityConfig.java
+++ 009-spring-gateway-reactive/src/main/java/org/defendev/spring/cloud/gateway/demo/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package org.defendev.spring.cloud.gateway.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
@@ -13,6 +15,8 @@ import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.core.oidc.IdTokenClaimNames;
 import org.springframework.security.oauth2.core.oidc.OidcScopes;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint;
+import org.springframework.security.web.server.util.matcher.PathPatternParserServerWebExchangeMatcher;
 
 import java.util.Collection;
 import java.util.List;
@@ -33,9 +37,42 @@ public class SecurityConfig {
 
     public static final String OAUTH2_REGISTRATION_ID_SPRING_AUTHZ_SERVER = "sprin6authz";
 
+    /*
+     * Whenever setting up a Reactive Gateway like this, ALWAYS anticipate that it will be run
+     * on multi-instance and ephemeral containers. Therefore, a random session terminations can alwyas
+     * happen and the application have to handle them gracefully. To make it possible,
+     * use proper authentication entry point depending on whether you're dealing with XHR request or
+     * HTML document request.
+     *
+     * Key design principle: the .oauth2Login() DSL is intended to be a black box
+     * that correctly configures the complex, dynamic behavior required for OAuth2 browser flows
+     * (including generating the correct dynamic redirect URI when multiple clients exist). Trying
+     * to manually override or extract components from it often breaks that carefully orchestrated logic,
+     * and is strongly discouraged.
+     *
+     * So, if I want to get 401 for XHRs in case of terminated session (instead of 302 to authorization server)
+     * the optimal choice is to have separate filter chain that doesn't even configure .oauth2Login().
+     *
+     * Additionally, remember to assure some SESSION AFFINITY on your ingress/load balancer!
+     *
+     */
+    @Order(0)
     @Bean
-    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity) {
-        return httpSecurity
+    public SecurityWebFilterChain xhrFilterChain(ServerHttpSecurity httpSecurity) {
+        final SecurityWebFilterChain webFilterChain = httpSecurity
+            .securityMatcher(new PathPatternParserServerWebExchangeMatcher("/confidential-spa/api/**"))
+            .authorizeExchange(customizer -> customizer.anyExchange().authenticated())
+            .exceptionHandling(customizer -> customizer
+                .authenticationEntryPoint(new HttpStatusServerEntryPoint(HttpStatus.UNAUTHORIZED))
+            )
+            .build();
+        return webFilterChain;
+    }
+
+    @Order(1)
+    @Bean
+    public SecurityWebFilterChain pagesFilterChain(ServerHttpSecurity httpSecurity) {
+        final SecurityWebFilterChain webFilterChain = httpSecurity
             .authorizeExchange(customizer -> customizer
                 .pathMatchers("/confidential-spa/session-invalidate").permitAll()
                 .pathMatchers("/confidential-spa/**").authenticated()
@@ -43,6 +80,7 @@ public class SecurityConfig {
             )
             .oauth2Login(Customizer.withDefaults())
             .build();
+        return webFilterChain;
     }
 
     @Bean
