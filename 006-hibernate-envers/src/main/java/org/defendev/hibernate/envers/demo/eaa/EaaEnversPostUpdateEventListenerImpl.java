223c614e54493c941fd0a727f8c5471552642f6a
Jozef Tokarski <jozef.tokarski@gmail.com>
2025-12-27 21:54:54 +0100

    006-hibernate-envers

--- 006-hibernate-envers/src/main/java/org/defendev/hibernate/envers/demo/eaa/EaaEnversPostUpdateEventListenerImpl.java
+++ 006-hibernate-envers/src/main/java/org/defendev/hibernate/envers/demo/eaa/EaaEnversPostUpdateEventListenerImpl.java
@@ -1,4 +1,35 @@
 package org.defendev.hibernate.envers.demo.eaa;
 
-public class EaaEnversPostUpdateEventListenerImpl {
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.envers.boot.internal.EnversService;
+import org.hibernate.envers.event.spi.EnversPostUpdateEventListenerImpl;
+import org.hibernate.event.spi.PostUpdateEvent;
+
+import static org.defendev.hibernate.envers.demo.eaa.EaaConditionalAuditUtil.notMarkedForAudit;
+
+
+
+public class EaaEnversPostUpdateEventListenerImpl extends EnversPostUpdateEventListenerImpl {
+
+    private static final Logger log = LogManager.getLogger();
+
+    public EaaEnversPostUpdateEventListenerImpl(EnversService enversService) {
+        super(enversService);
+    }
+
+    @Override
+    public void onPostUpdate(PostUpdateEvent event) {
+        final Object[] oldState = event.getOldState();
+        final Object[] newState = event.getState();
+        final String[] propertyNames = event.getPersister().getPropertyNames();
+
+        final boolean notMarkedForAudit = notMarkedForAudit(oldState, newState, propertyNames, event.getEntity());
+
+        if (notMarkedForAudit) {
+            return;
+        }
+        super.onPostUpdate(event);
+    }
+
 }
--- 006-hibernate-envers/src/main/java/org/defendev/hibernate/envers/demo/eaa/EaaEnversPreUpdateEventListenerImpl.java
+++ 006-hibernate-envers/src/main/java/org/defendev/hibernate/envers/demo/eaa/EaaEnversPreUpdateEventListenerImpl.java
@@ -1,4 +1,35 @@
 package org.defendev.hibernate.envers.demo.eaa;
 
-public class EaaEnversPreUpdateEventListenerImpl {
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.envers.boot.internal.EnversService;
+import org.hibernate.envers.event.spi.EnversPreUpdateEventListenerImpl;
+import org.hibernate.event.spi.PreUpdateEvent;
+
+import static org.defendev.hibernate.envers.demo.eaa.EaaConditionalAuditUtil.notMarkedForAudit;
+
+
+
+public class EaaEnversPreUpdateEventListenerImpl extends EnversPreUpdateEventListenerImpl {
+
+    private static final Logger log = LogManager.getLogger();
+
+    public EaaEnversPreUpdateEventListenerImpl(EnversService enversService) {
+        super(enversService);
+    }
+
+    @Override
+    public boolean onPreUpdate(PreUpdateEvent event) {
+        final Object[] oldState = event.getOldState();
+        final Object[] newState = event.getState();
+        final String[] propertyNames = event.getPersister().getPropertyNames();
+
+        final boolean notMarkedForAudit = notMarkedForAudit(oldState, newState, propertyNames, event.getEntity());
+
+        if (notMarkedForAudit) {
+            return false;
+        }
+        return super.onPreUpdate(event);
+    }
+
 }
